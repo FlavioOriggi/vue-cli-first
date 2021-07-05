90baf944ae1f3fe5c50445bd82e7ae04df992cbe
Flavio <flavio.origgi@outlook.it>
2021-07-05 23:45:50 +0200

    fatto bonus

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <div id="app">
-    <Title/>   
+  <div id="app">    
+    <Title/>    
+    <Hero/>
   </div>
 </template>
 
 <script>
 import Title from "./components/Title.vue";
+import Hero from "./components/Hero.vue";
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    Title
-  },
+    Title, 
+    Hero
+  }
 };
 </script>
+
+
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ test/src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="jumbotron">      
+  </div>
+</template>
+
+<script>
+export default {
+  item: "Hero",
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.jumbotron {  
+  background-image: url("../assets/Roma.jpg");
+  background-position: cover;
+  height: 500px;
+  background-repeat: no-repeat;
+}
+</style>
